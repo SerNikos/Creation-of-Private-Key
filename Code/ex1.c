2ca3f2e39b29261419d2fecc0a72335b4bf5f98b
SerNikos <78097143+SerNikos@users.noreply.github.com>
2021-07-31 14:36:08 +0300

    Update ex1.c

--- Code/ex1.c
+++ Code/ex1.c
@@ -1,4 +1,4 @@
-//NIKOLAOS SERGIS 18390173 PADA EX_1
+//NIKOLAOS SERGIS PADA EX_1
 
 #include <stdio.h> //basic library for input/output 
 #include <openssl/bn.h> //library that helps the computer to deal with big numbers and not cause overflow
